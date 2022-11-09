809be44d1baccf135c6881e03e33b2df196531f4
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-11-09 19:27:43 +0530

    MoodAnalyzerUC2

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -1,6 +1,10 @@
 package com.bridgelabz;
 
-
+class MoodAnalysisException extends Exception{
+    public MoodAnalysisException(String message){
+        super(message);
+    }
+}
 public class MoodAnalyzer {
     String message;
     public MoodAnalyzer(){
@@ -24,6 +28,15 @@ public class MoodAnalyzer {
         }
         return "";
         }
+        public String analyzeMoodE(String message){
+            if (message.contains("I am in Sad Mood")){
+                return "SAD";
+            }
+            if(message.contains("I am in Any Mood")){
+                return "HAPPY";
+            }
+            return "";
+        }
     public String analyzeMoodWithConstructor(){
 
         if (message.contains("I am in Sad Mood")){
@@ -33,5 +46,13 @@ public class MoodAnalyzer {
             return "HAPPY";
         }
         return "";
+    }
+    public void exceptionThrowing(String message) throws MoodAnalysisException{
+        try {
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+            moodAnalyzer.analyzeMood(message);
+        }catch (Exception e){
+            throw new MoodAnalysisException("Entered value is empty or null.");
+        }
     }
 }
--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -2,6 +2,7 @@ package com.bridgelabz;
 
 import org.junit.Assert;
 import org.junit.Test;
+
 public class MoodAnalyzerTest {
      @Test
     public void analyzeSadMessageTestWithConstructor(){
@@ -9,15 +10,14 @@ public class MoodAnalyzerTest {
         try {
         String mood = moodAnalyzer.analyzeMoodWithConstructor();
             Assert.assertEquals("SAD", mood);
-        }catch (Exception e){
+        }
+        catch (Exception e){
             System.out.println("Exception occurred in program : "+e);
         }
     }
-    @Test
-    public void analyzeTestMessageWithHandlingException(){
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-       String mood = moodAnalyzer.analyzeMood(null);
-        System.out.println(mood);
-    }
 
+    @Test
+   public void testUsingCustomExceptions(){
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+    }
 }
